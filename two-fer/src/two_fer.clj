c297b557137ab831a26a9e0974ff9f000f469df9
Michal <mikal82@protonmail.com>
2018-01-04 10:32:18 +0100

    Two-fer v2

--- two-fer/src/two_fer.clj
+++ two-fer/src/two_fer.clj
@@ -2,10 +2,5 @@
 
 (defn two-fer
   ([] (two-fer "you"))
-  ([name] (two-fer name "me"))
-  ([name me]
-    (str
-      (clojure.string/replace
-        (clojure.string/join ", " (map #(str "one for " %) [name me]))
-        #"^." #(clojure.string/upper-case %1)) "."))
+  ([name] (str "One for " name ", one for me."))
 )
