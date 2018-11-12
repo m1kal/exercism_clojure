6b2a7e817ee2afa43f63f252e3079da406af2e1c
m1kal <mikal82@protonmail.com>
2018-11-12 12:52:32 +0100

    Go-counting v2

--- go-counting/src/go_counting.clj
+++ go-counting/src/go_counting.clj
@@ -1,42 +1,31 @@
-(ns go-counting)
+(ns go-counting
+ (:require [clojure.set :refer [difference]]))
 
-(defn- neighbors [grid [x y]]
-  (for [i [-1 0 1] j [-1 0 1]
-        :let [x (+ x i) y (+ y j)]
-        :when (and (= 1 (+ (Math/abs i) (Math/abs j)))
-                   (get-in grid [y x]))]
-    [x y]))
+(defn- get-in-rev [grid]
+  (fn [[x y]] (get-in grid [y x])))
+
+(defn- neighbors [grid xy]
+  (filter (get-in-rev grid) (map #(map + xy %) [[0 1] [1 0] [0 -1] [-1 0]])))
+
+(defn- area-extension [grid area]
+  (difference (set (apply concat (map #(neighbors grid %) area))) area))
 
 (defn- find-area [grid position]
-  (if (= \space (get-in grid (reverse position)))
-    (loop [found #{position}]
-      (let [new-pos
-            (into found
-              (filter
-                #(= \space (get-in grid (reverse %)))
-                (apply concat (map #(neighbors grid %) found))))]
-        (if (= found new-pos)
-          new-pos
-          (recur new-pos))))
-    #{}))
+  (let [space? (fn [xy] (= \space ((get-in-rev grid) xy)))]
+    (loop [found (set (filter space? [position]))]
+      (let [new-pos (set (filter space? (area-extension grid found)))]
+        (if (empty? new-pos)
+          found
+          (recur (into found new-pos)))))))
 
 (defn- find-border [grid area]
-  (disj
-    (->> area
-         (map #(neighbors grid %))
-         (apply concat)
-         (map #(get-in grid (reverse %)))
-         set)
-    \space))
+  (set (map (get-in-rev grid) (area-extension grid area))))
 
 (defn territory [grid position]
-  (if (get-in grid (reverse position))
+  (if ((get-in-rev grid) position)
     (let [area (find-area grid position)
           border (find-border grid area)
-          owner (cond
-                  (not= 1 (count border)) nil
-                  (= \B (first border)) :black
-                  :else :white)]
+          owner (condp = border #{\W} :white #{\B} :black nil)]
       {:stones area :owner owner})
     (throw (AssertionError. "invalid coordinates"))))
 
@@ -48,8 +37,7 @@
        set))
 
 (defn territories [grid]
-  (let [t (for [y (range (count grid))
-                x (range (count (first grid)))]
+  (let [t (for [y (range (count grid)) x (range (count (first grid)))]
             (territory grid [x y]))]
     {:black-territory (find-territories t :black)
      :white-territory (find-territories t :white)
